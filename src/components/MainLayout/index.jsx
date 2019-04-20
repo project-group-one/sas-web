4cb5c4bbabbc4e7d3069d67e2f30634519484cff
yehq <yedi728@qq.com>
2019-04-20 17:08:34 +0800

    123

--- src/components/MainLayout/index.jsx
+++ src/components/MainLayout/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Search from '~/components/Search';
+import { Link } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
         <div className='home-header'>
             <div className='w'>
-                <img src='/asset/img/title.jpg' alt='title' />
+                <Link to={'/'}>
+                    <img src='/asset/img/title.jpg' alt='title' />
+                </Link>
                 <Search className='home-search' />
             </div>
         </div>
