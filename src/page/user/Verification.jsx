db10a24c4bfbf192cefb903c85cdde0117968726
yehq <yedi728@qq.com>
2019-12-07 00:53:32 +0800

    1

--- src/page/user/Verification.jsx
+++ src/page/user/Verification.jsx
@@ -34,65 +34,73 @@ class Detail extends Component {
       form: { getFieldDecorator },
       userStore
     } = this.props;
+    const getContent = () => {
+      switch (userStore.user.verifyStatus) {
+        case 0:
+          return (
+            <Form
+              layout="vertical"
+              onSubmit={this.handleSubmit}
+              hideRequiredMark
+            >
+              <FormItem label={"身份证正面"}>
+                {getFieldDecorator("frontPath", {
+                  rules: [
+                    {
+                      required: true,
+                      message: "请上传身份证正面"
+                    }
+                  ]
+                })(
+                  <UploadImage
+                    style={{
+                      display: "block",
+                      height: 200,
+                      width: 500,
+                      padding: 0
+                    }}
+                  />
+                )}
+              </FormItem>
+              <FormItem label={"身份证反面"}>
+                {getFieldDecorator("backPath", {
+                  rules: [
+                    {
+                      required: true,
+                      message: "请上传身份证反面"
+                    }
+                  ]
+                })(
+                  <UploadImage
+                    style={{
+                      display: "block",
+                      height: 200,
+                      width: 500,
+                      padding: 0
+                    }}
+                  />
+                )}
+              </FormItem>
+              <Button htmlType="submit" type="primary">
+                保存
+              </Button>
+            </Form>
+          );
+        case 1:
+          return <span style={{ color: "red" }}>审核中</span>;
+        case 2:
+          return <div style={{ color: "green" }}>身份已验证通过</div>;
+        default:
+          return "审核";
+      }
+    };
     return (
       <Card size="small" title="身份验证">
         <div
           style={{ display: "flex", padding: "0 50px" }}
           ref={this.getViewDom}
         >
-          <Left>
-            {userStore.user.verified ? (
-              <div style={{ color: "green" }}>身份已验证通过</div>
-            ) : (
-              <Form
-                layout="vertical"
-                onSubmit={this.handleSubmit}
-                hideRequiredMark
-              >
-                <FormItem label={"身份证正面"}>
-                  {getFieldDecorator("frontPath", {
-                    rules: [
-                      {
-                        required: true,
-                        message: "请上传身份证正面"
-                      }
-                    ]
-                  })(
-                    <UploadImage
-                      style={{
-                        display: "block",
-                        height: 200,
-                        width: 500,
-                        padding: 0
-                      }}
-                    />
-                  )}
-                </FormItem>
-                <FormItem label={"身份证反面"}>
-                  {getFieldDecorator("backPath", {
-                    rules: [
-                      {
-                        required: true,
-                        message: "请上传身份证反面"
-                      }
-                    ]
-                  })(
-                    <UploadImage
-                      style={{
-                        display: "block",
-                        height: 200,
-                        width: 500,
-                        padding: 0
-                      }}
-                    />
-                  )}
-                </FormItem>
-                <Button htmlType="submit" type="primary">
-                  保存
-                </Button>
-              </Form>
-            )}
-          </Left>
+          <Left>{getContent()}</Left>
         </div>
       </Card>
     );
