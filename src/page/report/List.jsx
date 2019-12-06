77cef18874abc9551fab1e30ec915b805dad53e8
quanshubli <309063267@qq.com>
2019-12-07 01:22:04 +0800

    fix

--- src/page/report/List.jsx
+++ src/page/report/List.jsx
@@ -1,80 +1,101 @@
-import React, { Component, Fragment } from 'react';
-import { observer, inject } from 'mobx-react';
-import { List, Divider, Pagination, Button } from 'antd';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import UploadModal from './UploadModal';
-import CheckModal from './CheckModal';
+import React, { Component, Fragment } from "react";
+import { observer, inject } from "mobx-react";
+import { List, Divider, Pagination, Button } from "antd";
+import { toJS } from "mobx";
+import styled from "styled-components";
+import UploadModal from "./UploadModal";
+import CheckModal from "./CheckModal";
 
 const Header = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `;
 const Extra = styled.div``;
 
 const Title = styled.h1`
-    font-size: 1.5em;
-    color: #105274;
+  font-size: 1.5em;
+  color: #105274;
 `;
 
-@inject('reportStore')
+@inject("reportStore", "userStore")
 @observer
 class ListPagination extends Component {
-    state = {
-        visible: false,
-        resultModalVisible: false,
-    };
-    componentDidMount() {
-        this.props.reportStore.getReportList();
-    }
+  state = {
+    visible: false,
+    resultModalVisible: false
+  };
+  componentDidMount() {
+    this.props.reportStore.getReportList();
+  }
 
-    handleCancel = () => {
-        this.setState({ visible: false });
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+  handleCheckModalCancel = () => {
+    this.setState({ resultModalVisible: false });
+  };
+  handleCheckResult = id => {
+    this.props.reportStore.getReport(id).then(() => {
+      this.setState({ resultModalVisible: true });
+    });
+  };
+  render() {
+    const {
+      reportStore: { data, queryParams },
+      userStore: { user }
+    } = this.props;
+    const { visible, resultModalVisible } = this.state;
+    const { current, pageSize } = queryParams;
+    const paginationProps = {
+      current,
+      pageSize
     };
-    handleCheckModalCancel = () => {
-        this.setState({ resultModalVisible: false });
-    };
-    handleCheckResult = id => {
-        this.props.reportStore.getReport(id).then(() => {
-            this.setState({ resultModalVisible: true })
-        })
-    }
-    render() {
-        const {
-            reportStore: { data, queryParams },
-        } = this.props;
-        const { visible, resultModalVisible } = this.state;
-        const { current, pageSize } = queryParams;
-        const paginationProps = {
-            current,
-            pageSize,
-        };
 
-        return (
-            <Fragment>
-                <Divider style={{ height: 6, margin: '2px 0', background: '#49a4f8' }} />
-                <List
-                    header={
-                        <Header>
-                            <Title>抽检报告</Title>
-                            <Extra>
-                                <Button onClick={() => this.setState({ visible: true })}>上传抽检报告</Button>
-                            </Extra>
-                        </Header>
-                    }
-                    footer={<Pagination {...paginationProps} />}
-                    dataSource={data.slice()}
-                    renderItem={(item) => (
-                        <List.Item actions={[<a onClick={() => this.handleCheckResult(item.id)}>查看检测结果</a>]}>
-                            <List.Item.Meta title={item.name} />
-                        </List.Item>
-                    )}
-                />
-                <UploadModal onCancel={this.handleCancel} key={visible.toString()} visible={visible} />
-                <CheckModal onCancel={this.handleCheckModalCancel} key={resultModalVisible.toString()} visible={resultModalVisible} />
-            </Fragment>
-        );
-    }
+    return (
+      <Fragment>
+        <Divider
+          style={{ height: 6, margin: "2px 0", background: "#49a4f8" }}
+        />
+        <List
+          header={
+            <Header>
+              <Title>抽检报告</Title>
+              {user.type === 1 && (
+                <Extra>
+                  <Button onClick={() => this.setState({ visible: true })}>
+                    上传抽检报告
+                  </Button>
+                </Extra>
+              )}
+            </Header>
+          }
+          footer={<Pagination {...paginationProps} />}
+          dataSource={data.slice()}
+          renderItem={item => (
+            <List.Item
+              actions={[
+                <a onClick={() => this.handleCheckResult(item.id)}>
+                  查看检测结果
+                </a>
+              ]}
+            >
+              <List.Item.Meta title={item.name} />
+            </List.Item>
+          )}
+        />
+        <UploadModal
+          onCancel={this.handleCancel}
+          key={visible.toString()}
+          visible={visible}
+        />
+        <CheckModal
+          onCancel={this.handleCheckModalCancel}
+          key={resultModalVisible.toString()}
+          visible={resultModalVisible}
+        />
+      </Fragment>
+    );
+  }
 }
 
 export default ListPagination;
